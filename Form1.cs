c3fe9bb9b89a1667638a82acbc45141d0e20b190
Nicole <124882220+nikkichep@users.noreply.github.com>
2024-02-12 09:37:16 +0300

    Editting username and password button

--- Form1.cs
+++ Form1.cs
@@ -4,7 +4,29 @@ namespace login{
         {
             InitializeComponemt();
         }
-        private void
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+
+        private bool isValid()
+        {
+            if (textBox1.Text.TrainStart() == string.Empty)
+            {
+                MessageBox.Show("Enter valid username", "error");
+                return false;
+            }else if (textBox2.Text.TrimStart() == string.Empty)
+            {
+                MessageBox.Show("enter valid password","error");
+                return false;
+            }
+            return true;
+        }
 
     }
 }
