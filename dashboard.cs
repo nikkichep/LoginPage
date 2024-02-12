7ea4c1897f95e9bfa8adf9849e500901d85215f4
Nicole <124882220+nikkichep@users.noreply.github.com>
2024-02-12 10:07:23 +0300

    initial commit

---
+++ dashboard.cs
@@ -0,0 +1,15 @@
+namespace login
+{
+    public partial class dashboard : Form
+    {
+        public dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
